670eed9ca742d854f620355e148abae79f25c60b
Amr Mohamed <amr_mohamed@live.com>
2017-03-19 22:30:03 +0200

    Add comment to script - Angle(deg not rad)

--- rotate.py
+++ rotate.py
@@ -2,6 +2,7 @@ import cv2
 
 
 def rotate(src, angle):
+    # Angle is in degrees
     rows, cols = src.shape
     rotation_matrix = cv2.getRotationMatrix2D((cols/2, rows/2), angle, 1)
     white = (255, 255, 255)
