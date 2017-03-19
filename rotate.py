4d9c2e5f769933f85681483010aed8ef0172bcb1
Mohamed Thabet <thabetx@gmail.com>
2017-03-19 18:40:19 +0200

    code cleanup

--- rotate.py
+++ rotate.py
@@ -1,9 +1,9 @@
-import numpy as np
 import cv2
 
+
 def rotate(src, angle):
-    rows,cols = src.shape
-    rotationMatrix = cv2.getRotationMatrix2D((cols/2,rows/2), angle, 1)
+    rows, cols = src.shape
+    rotation_matrix = cv2.getRotationMatrix2D((cols/2, rows/2), angle, 1)
     white = (255, 255, 255)
-    return cv2.warpAffine(src, rotationMatrix, (cols,rows), borderValue=white)
+    return cv2.warpAffine(src, rotation_matrix, (cols, rows), borderValue=white)
 
