a139f07a68fc264a3214bc8e7235202a613e96a7
Amr Mohamed <amr_mohamed@live.com>
2017-03-21 11:07:36 +0200

    Refactor the code and create new separate script for circular hough transform

---
+++ circhoughtrans.py
@@ -0,0 +1,22 @@
+import cv2
+
+def getCircles(img,minR,maxR)
+  '''Returns a list of circles (radius/center) '''
+	#circles = np.uint16(np.around(circles))
+	''' Add generated circles to an RGB image (cimg)
+	cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)	
+	for i in circles[0,:]:
+	  # draw the outer circle
+	  cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+	  # draw the center of the circle
+	  cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+	'''
+	return  cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=minR,maxRadius=maxR)
+
+def getAlignmentAngle(img):
+	img = cv2.medianBlur(img,5)
+	circles=getCircles(img,40,60)
+	delta_y=circles[0,1][1]-circles[0,0][1]
+	delta_x=circles[0,1][0]-circles[0,0][0]
+	angle=math.atan2(delta_y,delta_x)
+	return angle
