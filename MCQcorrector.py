26c07ece685587efce5586b440daef89e8c807ee
MohamedA-M-Hassan <anehmr0@gmail.com>
2017-03-22 14:19:01 +0200

    detect boundry || there is little error

--- MCQcorrector.py
+++ MCQcorrector.py
@@ -15,18 +15,33 @@ cv2.imshow('rotate',dst)
 
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-edges = cv2.Canny(gray, 400, 400)
+edges = cv2.Canny(gray, 100, 250)
 lines = cv2.HoughLinesP(edges, 1, np.pi/180, 25, minLineLength=100, maxLineGap=50)
 
 #hough = np.zeros(img.shape, np.uint8)
+
 hough = img
+
+rows = 3000
+columns= 5
+length = [[0 for x in range(columns)] for x in range(rows)]
+
+i=0
 for line in lines:
     x1, y1, x2, y2 = line[0]
-    cv2.line(hough, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    length[i][0]=np.sqrt(np.power((x1-x2),2)+np.power((y1-y2),2))
+    length[i][1]=x1
+    length[i][2]=y1
+    length[i][3]=x2
+    length[i][4]=y2
+    i=i+1
+    #cv2.line(hough, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    #length.append(np.sqrt(np.power((x1-x2),2)+np.power((y1-y2),2)))  
 
+length = sorted(length,key=lambda l:l[0], reverse=True)
+for  j in range(0,8):
+	cv2.line(hough, (length[j][1], length[j][2]), (length[j][3],length[j][4]), (0, 255, 0), 2)
 cv2.imwrite('hough.jpg', hough)
-
-
 
 cv2.namedWindow('hough.jpg', cv2.WINDOW_NORMAL) #show normal window
 cv2.imshow('hough.jpg', hough)
