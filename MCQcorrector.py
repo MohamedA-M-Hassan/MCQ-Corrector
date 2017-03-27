b0f78a78132e70261869c54f0f9c1bd8cf1133cf
MohamedA-M-Hassan <anehmr0@gmail.com>
2017-03-27 18:36:32 +0200

    convert to black and white || make dilation

--- MCQcorrector.py
+++ MCQcorrector.py
@@ -4,17 +4,29 @@ from rotate import rotate
 from detectLines import detectLines
 
 img = cv2.imread('S_2_hppscan111.png')
+cv2.namedWindow('original11', cv2.WINDOW_NORMAL) #show normal window
+cv2.imshow('original11', img)
 
-cv2.namedWindow('image', cv2.WINDOW_NORMAL) #show normal window
-cv2.imshow('image',img)
+
+img = cv2.medianBlur(img,5)
+ret,th1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+ret,thresh2 = cv2.threshold(img,225,255,cv2.THRESH_BINARY_INV)
+#th2 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,11,2)
+#th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+ #           cv2.THRESH_BINARY,11,2)
 
 
-test = detectLines(img)
+kernel = np.ones((20,20),np.uint8)
+dilation = cv2.dilate(thresh2,kernel,iterations = 2)
 
-#cv2.imwrite('hough.jpg', test)
+#test = detectLines(img)
+cv2.namedWindow('thershold_dilation.jpg', cv2.WINDOW_NORMAL) #show normal window
+cv2.imshow('thershold_dilation.jpg', dilation)
+cv2.namedWindow('original', cv2.WINDOW_NORMAL) #show normal window
+cv2.imshow('original', thresh2)
+cv2.imwrite('thershold.jpg',thresh2)
+cv2.imwrite('thershold_dilation.jpg',dilation)
 
-cv2.namedWindow('hough.jpg', cv2.WINDOW_NORMAL) #show normal window
-cv2.imshow('hough.jpg', test)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
