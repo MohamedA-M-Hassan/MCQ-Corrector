6f3ece343c11b6209747bfc5bd4cb5d1d664142d
Mohamed Thabet <thabetx@gmail.com>
2017-04-07 14:45:30 +0200

    Loop on dataset folder

--- MCQcorrector.py
+++ MCQcorrector.py
@@ -1,17 +1,22 @@
-import numpy as np
 import cv2
+import os
 from rotate import rotate
 from circhoughtrans import getAlignmentAngle
 from crop import crop
 
-img = cv2.imread('sample.png', 0)
-
-rotation_angle = getAlignmentAngle(img)
-rotated = rotate(img, rotation_angle)
-cropped = crop(rotated)
-result = cropped
-
 window = cv2.namedWindow('result',  flags=cv2.WINDOW_NORMAL)
-cv2.imshow('result', result)
-cv2.waitKey(0)
+directory = 'dataset/test/'
+
+for fn in os.listdir(directory):
+    name = directory + fn
+    img = cv2.imread(name, 0)
+    rotation_angle = getAlignmentAngle(img)
+    rotated = rotate(img, rotation_angle)
+    # cropped = crop(rotated)
+    result = rotated
+    cv2.imshow('result', result)
+    key = cv2.waitKey(0)
+    if key == ord('q'):
+        break
+
 cv2.destroyAllWindows()
