55caf141863dd7265a2f105fde2c8d1af09bef20
Mohamed Thabet <thabetx@gmail.com>
2017-04-07 10:01:16 +0200

    Hard coded cropping

--- MCQcorrector.py
+++ MCQcorrector.py
@@ -1,9 +1,15 @@
 import cv2
 from rotate import rotate
 
-window = cv2.namedWindow('result',  flags=cv2.WINDOW_NORMAL)
 img = cv2.imread('sample.png', 0)
-dst = rotate(img, 3)
-cv2.imshow('result', dst)
+
+rotated = rotate(img, 2)
+cropped = rotated[748:1406, 176:1034]
+result = cropped
+#result = rotated
+cv2.imwrite('result.png', result)
+
+window = cv2.namedWindow('result',  flags=cv2.WINDOW_NORMAL)
+cv2.imshow('result', result)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
