6a835da6c15c2eacb41a72e14887b7a3de9a66ef
Mohamed Thabet <thabetx@gmail.com>
2017-04-07 14:42:16 +0200

    Choose the left circle when cropping

--- crop.py
+++ crop.py
@@ -4,8 +4,11 @@ from circhoughtrans import getCircles
 
 def crop(src):
     blurred = cv2.medianBlur(src, 5)
-    circles = getCircles(blurred,40,60)
-    x = circles[0,0][1]
-    y = circles[0,0][0]
+    circles = getCircles(blurred, 40, 60)
+    left_circle = circles[0, 0]
+    if left_circle[0] > circles[0, 1, 0]:
+        left_circle = circles[0, 1]
+    x = int(left_circle[1])
+    y = int(left_circle[0])
     cropped = src[x-800:x-145, y-35:y+825]
     return cropped
