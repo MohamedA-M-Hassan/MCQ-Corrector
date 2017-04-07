8b37c533a81ca11aaa0d58bf7ee31821499d88b8
Amr Mohamed <amr_mohamed@live.com>
2017-04-07 10:58:26 +0200

    Adjust main script to fix the alignment of the image

--- MCQcorrector.py
+++ MCQcorrector.py
@@ -1,7 +1,13 @@
 import numpy as np
 import cv2
+from rotate import rotate
+from circhoughtrans import getAlignmentAngle
 
-img = cv2.imread('S_1_hppscan1.png',0)
-cv2.imshow('image',img)
+window = cv2.namedWindow('result',  flags=cv2.WINDOW_NORMAL)
+img = cv2.imread('sample.png', 0)
+rotation_angle = getAlignmentAngle(img)
+print(rotation_angle)
+dst = rotate(img, rotation_angle)
+cv2.imshow('result', dst)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
