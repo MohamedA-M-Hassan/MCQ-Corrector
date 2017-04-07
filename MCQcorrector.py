3e473f9aace1c60081df9d06009207d585d32c2e
MohamedA-M-Hassan <anehmr0@gmail.com>
2017-04-07 14:48:07 +0200

    merge with Amr

--- MCQcorrector.py
+++ MCQcorrector.py
@@ -1,31 +1,20 @@
+from calendar import different_locale
+
 import cv2
 import numpy as np
 from rotate import rotate
 from detectLines import detectLines
+from detectLines import dilation
 
 img = cv2.imread('S_2_hppscan111.png')
-cv2.namedWindow('original11', cv2.WINDOW_NORMAL) #show normal window
-cv2.imshow('original11', img)
 
+test = dilation(img)
+test2 = detectLines(test)
+cv2.namedWindow('lines.jpg', cv2.WINDOW_NORMAL) #show normal window
+cv2.imshow('lines.jpg', test)
 
-img = cv2.medianBlur(img,5)
-ret,th1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
-ret,thresh2 = cv2.threshold(img,225,255,cv2.THRESH_BINARY_INV)
-#th2 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,11,2)
-#th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
- #           cv2.THRESH_BINARY,11,2)
-
-
-kernel = np.ones((20,20),np.uint8)
-dilation = cv2.dilate(thresh2,kernel,iterations = 2)
-
-#test = detectLines(img)
-cv2.namedWindow('thershold_dilation.jpg', cv2.WINDOW_NORMAL) #show normal window
-cv2.imshow('thershold_dilation.jpg', dilation)
-cv2.namedWindow('original', cv2.WINDOW_NORMAL) #show normal window
-cv2.imshow('original', thresh2)
-cv2.imwrite('thershold.jpg',thresh2)
-cv2.imwrite('thershold_dilation.jpg',dilation)
+#cv2.imwrite('thershold.jpg',thresh2)
+#cv2.imwrite('thershold_dilation.jpg',dilation)
 
 
 cv2.waitKey(0)
